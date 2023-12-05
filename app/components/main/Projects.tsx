0ee9594ce378252a80e388d11fcaf9c4f1d405f1
Marko Sudar <mjsudar21@catawba.edu>
2023-12-05 18:53:59 -0500

    phone beta v5

--- app/components/main/Projects.tsx
+++ app/components/main/Projects.tsx
@@ -4,10 +4,10 @@ import ProjectCard from "../sub/ProjectCard";
 const Projects = () => {
   return (
     <div className="flex flex-col items-center justify-center" id="projects">
-      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-10">
+      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
         My Projects
       </h1>
-      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
+      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10 py-10">
         <ProjectCard
           src="/NextWebsite.png"
           title="Modern Next.js Portfolio"
