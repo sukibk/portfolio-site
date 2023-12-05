f96f4c8e399bab08f4859b5a103fed2852dbd02d
Marko Sudar <mjsudar21@catawba.edu>
2023-12-05 18:44:21 -0500

    phone beta v5

--- app/components/main/Projects.tsx
+++ app/components/main/Projects.tsx
@@ -3,11 +3,8 @@ import ProjectCard from "../sub/ProjectCard";
 
 const Projects = () => {
   return (
-    <div
-      className="flex flex-col items-center justify-center py-20"
-      id="projects"
-    >
-      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
+    <div className="flex flex-col items-center justify-center" id="projects">
+      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-10">
         My Projects
       </h1>
       <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
