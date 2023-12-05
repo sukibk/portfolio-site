14f04428e9e750ee7d7c430d250dd737c918fb2d
Marko Sudar <mjsudar21@catawba.edu>
2023-12-05 02:38:50 -0500

    v alpha 0.2-mobile

--- app/components/main/Encryption.tsx
+++ app/components/main/Encryption.tsx
@@ -10,7 +10,7 @@ const Encryption = () => {
     <div className="flex flex-row relative items-center justify-center min-h-screen w-full h-full">
       <div className="absolute w-auto h-auto top-0 z-[5]">
         <motion.div
-          variants={slideInFromTop}
+          variants={slideInFromTop()}
           className="text-[40px] font-medium text-center text-gray-200"
         >
           Performance
